4470ff9c4d8e98aa9a27ef5501a87015733a108c
Martijn van den Boogaard <martijn.vandenboogaard@gmail.com>
2016-03-09 17:08:29 +1100

    Working docker container based on Alpine linux

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,15 @@
-FROM ubuntu:14.04
-RUN sudo apt-get update && sudo apt-get install -y --force-yes default-jre mysql-client
+FROM alpine:3.2
 
-COPY UnlimitedJCEPolicy /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/
 COPY domibus /opt/domibus
 COPY config/init.sh /opt/domibus/bin/
 
-ENV JAVA_HOME /usr/lib/jvm/java-7-openjdk-amd64/jre
-
 # Domibus MySql hostname
-ENV DMBSQLSVR mysql
+ENV DMBSQLSVR localhost
 
 # Domibus MySql root password
-ENV DMBSQLPWD links6:toner
+ENV DMBSQLPWD password
+
+ENV JAVA_HOME /usr/lib/jvm/default-jvm/jre
 
 # Open ports to the container
 
@@ -28,9 +26,20 @@ EXPOSE 5445
 EXPOSE 5455
 
 VOLUME /opt/domibus/pmodes
-VOLUME /opt/domibues/modules/eu/domibus/main
+VOLUME /opt/domibus/modules/eu/domibus/main
 
+RUN ["apk", "update"]
+RUN ["apk", "add", "openjdk7"]
+RUN apk add mysql mysql-client && \
+	mv /usr/share/mysql/mysql.server /etc/init.d/mysql && \
+	/usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql && \
+	service -i mysql start && \
+    /usr/bin/mysqladmin -u root password password && \
+    service -i mysql stop
+RUN ["apk", "add", "bash"]
 RUN ["/bin/chmod", "750", "/opt/domibus/bin/init.sh"]
+
+COPY UnlimitedJCEPolicy ${JAVA_HOME}/lib/
 
 ENTRYPOINT ["/opt/domibus/bin/init.sh"]
 
--- config/init.sh
+++ config/init.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+if ! service mysql status; then
+	service mysql start
+fi
+
+if [ -f /opt/domibus/bin/init.done ]; then exit 1; fi
+
 # Initialise mysql database
-/usr/bin/mysql -h $DMBSQLSVR -u root --password=$DMBSQLPWD -e "drop schema if exists edelivery;create schema edelivery;alter database edelivery charset=utf8; create user edelivery identified by 'edelivery';grant all on edelivery.* to edelivery;"
+/usr/bin/mysql -h localhost -u root --password=password -e "drop schema if exists edelivery;create schema edelivery;alter database edelivery charset=utf8; create user 'edelivery'@'localhost' identified by 'edelivery';grant all on edelivery.* to edelivery;"
 
 # Create domibus database
-mysql -h $DMBSQLSVR -u root --password=$DMBSQLPWD edelivery < /opt/domibus/sql-scripts/create-mysql.sql
+mysql -h localhost -u root --password=password edelivery < /opt/domibus/sql-scripts/create-mysql.sql
 
 # Set the mysql hostname
-/bin/sed -i.bak 's/jdbc:mysql:\/\/localhost/jdbc:mysql:\/\/'"$DMBSQLSVR"'/' /opt/domibus/standalone/configuration/standalone.xml
+# /bin/sed -i.bak 's/jdbc:mysql:\/\/localhost/jdbc:mysql:\/\/'"$DMBSQLSVR"'/' /opt/domibus/standalone/configuration/standalone.xml
 
 sh /opt/domibus/bin/standalone.sh
+
+touch /opt/domibus/bin/init.done
